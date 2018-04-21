2d4e6ce67d5ff2a039184956a03afc7beccb79b0
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2018-04-21 12:12:30 +0300

    Added add view controller add icons

---
+++ mnhani/addViewController.swift
@@ -0,0 +1,35 @@
+//
+//  addViewController.swift
+//  mnhani
+//
+//  Created by Abuzer Emre Osmanoğlu on 21.04.2018.
+//  Copyright © 2018 Abuzer Emre Osmanoğlu. All rights reserved.
+//
+
+import UIKit
+
+class addViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
