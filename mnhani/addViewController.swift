dcdeb6b5533157952827b483ddd1ed13d274e389
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2018-05-21 15:51:03 +0300

    Material

--- mnhani/addViewController.swift
+++ mnhani/addViewController.swift
@@ -14,11 +14,12 @@ import MaterialComponents
 class addViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var titleTextField: MDCTextField!
     @IBOutlet weak var zoneTextField: UITextField!
     @IBOutlet weak var mgrsZoneTextField: UITextField!
     @IBOutlet weak var eastTextField: UITextField!
     @IBOutlet weak var northTextField: UITextField!
+    var allTextFieldControllers = [MDCTextInputControllerFloatingPlaceholder]()
     var toolbar = FormToolbar()
     var locationManager = CLLocationManager()
     var userLocation = CLLocation()
@@ -26,15 +27,13 @@ class addViewController: UIViewController, UITextFieldDelegate, CLLocationManage
     var longitude = Double()
     var mgrs = String()
     
-
+    
+    @IBOutlet weak var doneButton: MDCFloatingButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleTextField.delegate = self
-        zoneTextField.delegate = self
-        mgrsZoneTextField.delegate = self
-        eastTextField.delegate = self
-        northTextField.delegate = self
+        setupTextFields()
         
         self.toolbar = FormToolbar(inputs: [titleTextField, eastTextField, northTextField, zoneTextField, mgrsZoneTextField])
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapView(gesture:)))
@@ -47,6 +46,7 @@ class addViewController: UIViewController, UITextFieldDelegate, CLLocationManage
     }
     
     
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         userLocation = locations[0]
         
@@ -56,18 +56,32 @@ class addViewController: UIViewController, UITextFieldDelegate, CLLocationManage
         mgrs = (converter?.mgrs(fromLatitude: latitude, longitude: longitude))!
         let zones = mgrs.prefix(5)
         let coordinates = mgrs.suffix(11)
-        zoneTextField.placeholder = String(zones.prefix(3))
-        mgrsZoneTextField.placeholder = String(zones.suffix(2))
+        zoneTextField.placeholder = String(zones.prefix(2))
+        mgrsZoneTextField.placeholder = String(zones.suffix(3))
         eastTextField.placeholder = String(coordinates.prefix(5))
         northTextField.placeholder = String(coordinates.suffix(5))
+        
+    }
+    
+    //MARK: - Keyboard and Text Field Attributes
+    func setupTextFields() {
+        titleTextField.delegate = self
+        zoneTextField.delegate = self
+        mgrsZoneTextField.delegate = self
+        eastTextField.delegate = self
+        northTextField.delegate = self
+        
+        let titleController = MDCTextInputControllerOutlined(textInput: titleTextField)
+        titleController.placeholderText = NSLocalizedString("PointName", comment: "")
+        allTextFieldControllers.append(titleController)
+        
         let formatter = DateFormatter()
         formatter.dateStyle = .short
         formatter.timeStyle = .medium
         let timeString = formatter.string(from: Date())
-        titleTextField.placeholder = timeString
+        titleTextField.text = timeString
     }
     
-    //MARK: - Keyboard and Text Field Attributes
     func textFieldDidBeginEditing(_ textField: UITextField) {
         toolbar.update()
     }
@@ -129,7 +143,7 @@ class addViewController: UIViewController, UITextFieldDelegate, CLLocationManage
             northTextField.becomeFirstResponder()
         case (northTextField, 5):
             zoneTextField.becomeFirstResponder()
-        case (zoneTextField, 3):
+        case (zoneTextField, 2):
             mgrsZoneTextField.becomeFirstResponder()
         default:
             break
@@ -145,50 +159,82 @@ class addViewController: UIViewController, UITextFieldDelegate, CLLocationManage
     
     // MARK: - Buttons
     @IBAction func saveButton(_ sender: Any) {
-        var title = titleTextField.text
+        let title = titleTextField.text
         var zone = zoneTextField.text
         var mgrsZone = mgrsZoneTextField.text
         var east = eastTextField.text
         var north = northTextField.text
-        
-        if title == "" {
-            title = titleTextField.placeholder
-        }
-        if zone == "" {
-            zone = zoneTextField.placeholder
-        }
-        if mgrsZone == "" {
-            mgrsZone = mgrsZoneTextField.placeholder
-        }
-        if east == "" {
-            east = eastTextField.placeholder
-        }
-        if north == "" {
-            north = northTextField.placeholder
-        }
-        
-        
-        let mgrsText = "\(zone!)\(mgrsZone!) \(east!) \(north!)"
-        
-        let mgrsTextLatitude = UnsafeMutablePointer<Double>.allocate(capacity: 1)
-        let mgrsTextLongitude = UnsafeMutablePointer<Double>.allocate(capacity: 1)
-        
-        
-        let converter = GeoCoordinateConverter.shared()
-        _ = converter?.mgrs(mgrsText, toLatitude: mgrsTextLatitude, longitude: mgrsTextLongitude)
-        
-        CoreDataManager.store(title: title!, mgrs: mgrsText, latitude: mgrsTextLatitude[0], longitude: mgrsTextLongitude[0])
-        
         let message = MDCSnackbarMessage()
-        message.text = NSLocalizedString("Saved", comment: "")
         MDCSnackbarManager.setBottomOffset(50)
-        MDCSnackbarManager.show(message)
         
-        mgrsTextLatitude.deallocate()
-        mgrsTextLongitude.deallocate()
-        
-        NotificationCenter.default.post(name: NSNotification.Name("Update"), object: nil)
-        _ = navigationController?.popViewController(animated: true)
+        if title?.count == 0 {
+            message.text = NSLocalizedString("TitleError", comment: "")
+            MDCSnackbarManager.show(message)
+            titleTextField.shake()
+        } else if zone?.count == 1 {
+            message.text = NSLocalizedString("ZoneError", comment: "")
+            MDCSnackbarManager.show(message)
+            zoneTextField.shake()
+        } else if mgrsZone?.count == 1 || mgrsZone?.count == 2 {
+            message.text = NSLocalizedString("MgrsZoneError", comment: "")
+            MDCSnackbarManager.show(message)
+            mgrsZoneTextField.shake()
+        } else if (east?.count)! > 0 && (east?.count)! < 5 {
+            message.text = NSLocalizedString("CoordinateError", comment: "")
+            MDCSnackbarManager.show(message)
+            eastTextField.shake()
+        } else if (north?.count)! > 0 && (north?.count)! < 5 {
+            message.text = NSLocalizedString("CoordinateError", comment: "")
+            MDCSnackbarManager.show(message)
+            northTextField.shake()
+        } else {
+            if zone?.count == 0 {
+                zone = zoneTextField.placeholder
+            }
+            if mgrsZone?.count == 0 {
+                mgrsZone = mgrsZoneTextField.placeholder
+            }
+            if east?.count == 0 {
+                east = eastTextField.placeholder
+            }
+            if north?.count == 0 {
+                north = northTextField.placeholder
+            }
+            
+            let mgrsText = "\(zone!)\(mgrsZone!) \(east!) \(north!)"
+            
+            let mgrsTextLatitude = UnsafeMutablePointer<Double>.allocate(capacity: 1)
+            let mgrsTextLongitude = UnsafeMutablePointer<Double>.allocate(capacity: 1)
+            
+            
+            let converter = GeoCoordinateConverter.shared()
+            _ = converter?.mgrs(mgrsText, toLatitude: mgrsTextLatitude, longitude: mgrsTextLongitude)
+            
+            CoreDataManager.store(title: title!, mgrs: mgrsText, latitude: mgrsTextLatitude[0], longitude: mgrsTextLongitude[0])
+            
+            message.text = NSLocalizedString("Saved", comment: "")
+            MDCSnackbarManager.show(message)
+            
+            mgrsTextLatitude.deallocate()
+            mgrsTextLongitude.deallocate()
+            
+            NotificationCenter.default.post(name: NSNotification.Name("Update"), object: nil)
+            _ = navigationController?.popViewController(animated: true)
+        }
     }
+}
 
+public extension UIView {
+    
+    func shake(count : Float = 5, for duration : TimeInterval = 0.3, withTranslation translation : Float = 5) {
+        
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.repeatCount = count
+        animation.duration = duration/TimeInterval(animation.repeatCount)
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: CGFloat(-translation), y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: CGFloat(translation), y: self.center.y))
+        layer.add(animation, forKey: "shake")
+    }
 }
