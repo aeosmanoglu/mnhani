49e5d1d6cf2bce65d79d79552c3c2e614a250de8
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2018-05-28 13:23:25 +0300

    Added

---
+++ mnhani/CustomPointAnnotationView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomPointAnnotationView.swift
+//  mnhani
+//
+//  Created by Abuzer Emre Osmanoğlu on 28.05.2018.
+//  Copyright © 2018 Abuzer Emre Osmanoğlu. All rights reserved.
+//
+
+import Mapbox
+
+class CustomPointAnnotationView: MGLPointAnnotation {
+    var willUseImage: Bool = false
+}
